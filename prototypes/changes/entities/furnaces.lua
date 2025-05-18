3226a139f6c0ffdd6f813a5b660ba0b276d2071c
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-05-18 20:21:34 +0100

    changed furnaces to have half energy usage, the other half being their drain - lowered module slots in *electric* furnaces

---
+++ prototypes/changes/entities/furnaces.lua
@@ -0,0 +1,12 @@
+data.raw["furnace"]["electric-furnace"].module_slots = 5
+data.raw["furnace"]["electric-furnace"].energy_usage = "50kW"
+data.raw["furnace"]["electric-furnace"].energy_source.drain = "50kW"
+
+data.raw["furnace"]["pm-electric-furnace-2"].module_slots = 8
+data.raw["furnace"]["pm-electric-furnace-2"].energy_usage = "100kW"
+data.raw["furnace"]["pm-electric-furnace-2"].energy_source.drain = "100kW"
+data.raw["furnace"]["pm-electric-furnace-2"].icons_positioning =
+{
+    {inventory_index = defines.inventory.furnace_modules, shift = {0, 1}, multi_row_initial_height_modifier = -0.3, max_icons_per_row = 4, scale = 0.42}
+},
+
